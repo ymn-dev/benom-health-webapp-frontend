f4a70a9effa219d6da1872f25320d84b80bfa856
sukandasaeieo <sukandasaeieo@gmail.com>
2023-09-15 09:54:24 +0700

    edit1

--- src/main.jsx
+++ src/main.jsx
@@ -106,13 +106,12 @@ const router = createBrowserRouter([
   {
     path: "/home",
     element: (
-      <>
-        <Layout>
-          <Dashboard />
-        </Layout>
-      </>
-    ),
-  },
+      <Layout>
+      <Hero />
+      <CardContainer />
+    </Layout>
+  ),
+},
   {
     path: "/profile",
     element: (
