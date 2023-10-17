8268af973993a968e2adf1c93c29e75251f849a9
KanomWaan <totofluk1412@gmail.com>
2023-10-17 20:34:52 +0700

    finish function before decorate

--- src/Component/User/EditProfile.jsx
+++ src/Component/User/EditProfile.jsx
@@ -2,18 +2,76 @@ import React, { useEffect, useState } from "react";
 import { useLoginContext } from "../../Context/LoginContext";
 import { Link } from "react-router-dom";
 import editIcon from "../../assets/edit-svgrepo-com.svg";
+import axios from "axios";
 
 const EditProfile = () => {
   const { login, user, setUser } = useLoginContext();
   //these are the only field we let them edit
-  const [editUser, setEditUser] = useState({});
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [newProfilePicture, setNewProfilePicture] = useState("");
+  // const [editUser, setEditUser] = useState({});
+  
+  // const [ProfileImg, setProfileImg] = useState("");
+  // const [ProfileURL, setProfileURL] = useState("https://i.ibb.co/mHF9LZZ/venom-Cheese.png");
+  const [reload, setReload] = useState(false);
+  const [firstName, setfirstName] = useState("");
+  const [lastName, setlastName] = useState("");
+  const [gender, setgender] = useState("");
+  const [birthday, setbirthday] = useState("");
+  const [height, setheight] = useState("");
+  const [weight, setweight] = useState("");
 
-  const submitHandler = (ev) => {
+  
+   // ส่วนต่อ backend 
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(`https://benom-backend.onrender.com/users/${user._id}`, { headers: user.headers });
+        const { /*profilePicture,*/firstName,lastName,gender,birthday,height,weight } = response.data.data;
+        setUser({ ...user,/*profilePicture,*/ firstName,lastName,gender,birthday,height,weight});
+      } catch (error) {
+        console.error("Error update User", error);
+      }
+    };
+
+    getData();
+  }, [reload]);
+
+  // const submitIMG = async (ev) => {
+  //   ev.preventDefault();
+  //   if (!ProfileImg) return;
+
+  //   try {
+  //     const response = await axios.patch(`https://benom-backend.onrender.com/users/${user._id}`, {profilePicture: ProfileImg},{ headers: user.headers });
+  //     if (response.status === 200) {
+  //       setReload(!reload);
+       
+  //     }
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
+
+  const submitHandler = async (ev) => {
     ev.preventDefault();
-    // ใช้ URL รูปภาพใหม่จาก state ในการอัปเดต editUser
-    setUser({ ...user, ...editUser, profilePicture: newProfilePicture });
+    const dataUser = {
+      ...user,
+      firstName,
+      lastName,
+      gender,
+      birthday,
+      height,
+      weight,
+    };
+    
+    // setUser({ ...user, ...editUser, profilePicture: ProfileImg });
+    try {
+      const response = await axios.patch(`https://benom-backend.onrender.com/users/${user._id}`, dataUser ,{ headers: user.headers });
+      if (response.status === 200) {
+        setReload(!reload);
+       
+      }
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   if (!login) {
@@ -38,62 +96,67 @@ const EditProfile = () => {
     marginLeft: "10px",
   };
 
-  const editButtonStyle = {
-    position: "absolute",
-    bottom: "0",
-    right: "0",
-    backgroundColor: "transparent",
-    border: "none",
-    cursor: "pointer",
-  };
+  // const editButtonStyle = {
+  //   position: "absolute",
+  //   bottom: "0",
+  //   right: "0",
+  //   backgroundColor: "transparent",
+  //   border: "none",
+  //   cursor: "pointer",
+  // };
 
-  const editIconStyle = {
-    width: "30px",
-    height: "30px",
-  };
+  // const editIconStyle = {
+  //   width: "30px",
+  //   height: "30px",
+  // };
 
   return (
     <form onSubmit={submitHandler}>
       <div className="md:w-1/2 mx-auto">
         <div className="bg-salmon p-4  rounded-t-lg ">
           <div className="flex justify-between items-center">
+
+        
             <div className="relative">
-              <img src={user.profilePicture} alt="Profile" style={profileImageStyle} />
-              {/* แสดงปุ่มและเปิด modal เมื่อกด */}
-              <button type="button" onClick={() => setIsModalOpen(true)} style={editButtonStyle}>
-                <img src={editIcon} style={editIconStyle} alt="editIcon" />
-              </button>
-            </div>
+              <img src={user.profilePicture} alt="Profile" className="w-48 h-48 rounded-full" />
+             
+              {/* ส่วน modal update รูปภาพ 
+              <button style={editButtonStyle} onClick={()=>document.getElementById('my_modal_1').showModal()}>
+              <img src={editIcon} style={editIconStyle} alt="editIcon" /></button>
+                  <dialog id="my_modal_1" className="modal">
+                  <div className="modal-box">
+
+                  <h2 className="text-salmon font-bold text-2xl mb-5">Add New Profile Picture</h2>
+                  <img src={ProfileImg || ProfileURL} alt="venom-PROFILE" className="rounded-lg w-full h-auto" />
+                  <div className="field-value-pair mt-5">
+                  <labels>Enter image URL : </labels> 
+                  <input type="text" name="profilePicture" placeholder="image URL" value={ProfileImg} onChange={(ev) => setProfileImg(ev.target.value)} style={inputStyle} />
+                    
+                  </div>
+                   
+                  <div className="modal-action">
+                  <button onClick={submitIMG} className="btn hover:btn-success" disabled={!ProfileImg}>
+                        Update
+                  </button>
+                  <form method="dialog">
+                   if there is a button in form, it will close the modal
+                   <button className="btn btn-circle hover:btn-error">
+                          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                          </svg>
+                    </button>
+                  </form>
+                  </div>
+                  </div>
+                  </dialog> */}
+              </div>
             <div className="text-white font-bold text-2xl">
               <h1>Edit your Profile Here</h1>
             </div>
           </div>
         </div>
 
-        {/* แสดง modal เมื่อ isModalOpen เป็น true */}
-        {isModalOpen && (
-          <div className="bg-white p-4 rounded-b-lg pt-5 md:flex md:flex-row">
-            <div className="md:flex-1">
-              {/* ให้ผู้ใช้กรอก URL รูปภาพใหม่ */}
-              <h2 className="text-salmon font-bold text-2xl mb-5">Add New Profile Picture</h2>
-              <div className="field-value-pair">
-                <p>
-                  <input type="url" name="profilePicture" placeholder="New image URL here" value={newProfilePicture} onChange={(ev) => setNewProfilePicture(ev.target.value)} style={inputStyle} />
-                </p>
-              </div>
-
-              {/* ปุ่ม "Save" และ "Cancel" ใน modal */}
-              <div>
-                <button type="submit" className="bg-salmon hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5">
-                  Save
-                </button>
-                <button type="button" onClick={() => setIsModalOpen(false)} className="bg-salmon hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5 ml-5">
-                  Cancel
-                </button>
-              </div>
-            </div>
-          </div>
-        )}
+        
 
         <div className="bg-white p-4 rounded-b-lg pt-5 md:flex md:flex-row">
           <div className="md:flex-1">
@@ -101,29 +164,39 @@ const EditProfile = () => {
             <div className="field-value-pair">
               <p>
                 <span className="text-black">First name</span>
+                {user.firstName ? (
+                  <span className="text-salmon">{user.firstName}</span>
+                ) : (
                 <input
                   type="text"
-                  placeholder={"Add first name" || user.firstName}
+                  placeholder={"Add first name only once time" || user.firstName}
                   name="firstName"
                   id="firstName"
+                  value={firstName}
                   onChange={(ev) => {
-                    setEditUser({ ...editUser, [ev.target.name]: ev.target.value });
+                    setfirstName(ev.target.value);
                   }}
                   style={inputStyle}
                 />
+                )}
               </p>
               <p>
                 <span className="text-black">Last name</span>
+                {user.lastName ? (
+                  <span className="text-salmon">{user.lastName}</span>
+                ) : (
                 <input
                   type="text"
-                  placeholder={"Add last name" || user.lastName}
+                  placeholder={"Add last name only once time" || user.lastName}
                   name="lastName"
                   id="lastName"
+                  value={lastName}
                   onChange={(ev) => {
-                    setEditUser({ ...editUser, [ev.target.name]: ev.target.value });
+                    setlastName(ev.target.value);
                   }}
                   style={inputStyle}
                 />
+                )}
               </p>
               <p>
                 <span className="text-black mr-5 ">Gender</span>
@@ -137,7 +210,7 @@ const EditProfile = () => {
                         name="gender"
                         value="Male"
                         onChange={(ev) => {
-                          setEditUser({ ...editUser, [ev.target.name]: ev.target.value });
+                          setgender(ev.target.value);
                         }}
                       />
                       Male
@@ -148,7 +221,7 @@ const EditProfile = () => {
                         name="gender"
                         value="Female"
                         onChange={(ev) => {
-                          setEditUser({ ...editUser, [ev.target.name]: ev.target.value });
+                          setgender(ev.target.value);
                         }}
                       />
                       Female
@@ -165,8 +238,10 @@ const EditProfile = () => {
                     type="date"
                     name="birthday"
                     id="birthday"
+                    placeholder={"Add birthday only once time"}
+                    value={birthday}
                     onChange={(ev) => {
-                      setEditUser({ ...editUser, [ev.target.name]: ev.target.value });
+                      setbirthday(ev.target.value);
                     }}
                     style={inputStyle}
                   />
@@ -183,8 +258,9 @@ const EditProfile = () => {
                   name="height"
                   id="height"
                   placeholder="Height (cm)"
+                  value={height}
                   onChange={(ev) => {
-                    setEditUser({ ...editUser, [ev.target.name]: ev.target.value });
+                    setheight(ev.target.value);
                   }}
                   style={inputStyle}
                 />
@@ -196,12 +272,14 @@ const EditProfile = () => {
                   name="weight"
                   id="weight"
                   placeholder="Weight (kg)"
+                  value={weight}
                   onChange={(ev) => {
-                    setEditUser({ ...editUser, [ev.target.name]: ev.target.value });
+                    setweight(ev.target.value);
                   }}
                   style={inputStyle}
                 />
               </p>
+              
             </div>
 
             {/* <Link to="/profile"> */}
@@ -246,6 +324,7 @@ const EditProfile = () => {
               <br />
               <span className="text-salmon">{user.liveCaloriesBurned}</span>
             </p> */}
+
           </div>
         </div>
       </div>
--- src/Component/User/Profile.jsx
+++ src/Component/User/Profile.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLoginContext } from "../../Context/LoginContext";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import defaultPicture from "../../assets/Emily_profile_icon.png";
+import editIcon from "../../assets/edit-svgrepo-com.svg";
 
 const Profile = () => {
   const { login, user } = useLoginContext();
@@ -11,42 +11,115 @@ const Profile = () => {
     navigate("/signin");
   }
 
-  // const profileImageStyle = {
-  //   width: '200px',
-  //   height: '200px',
-  //   borderRadius: '50%',
-  //   position: 'relative',
-  //   top: '80px',
-  //   left: '40px',
-  // };
+  const [ProfileImg, setProfileImg] = useState("");
+  const [ProfileURL, setProfileURL] = useState("https://i.ibb.co/mHF9LZZ/venom-Cheese.png");
+  const [reload, setReload] = useState(false);
+ // ส่วนต่อ backend 
+ useEffect(() => {
+  const getData = async () => {
+    try {
+      const response = await axios.get(`https://benom-backend.onrender.com/users/${user._id}`, { headers: user.headers });
+      const { profilePicture } = response.data.data;
+      setUser({ ...user, profilePicture});
+    } catch (error) {
+      console.error("Error update User", error);
+    }
+  };
+
+  getData();
+}, [reload]);
+
+const submitIMG = async (ev) => {
+  ev.preventDefault();
+  if (!ProfileImg) return;
+
+  try {
+    const response = await axios.patch(`https://benom-backend.onrender.com/users/${user._id}`, {profilePicture: ProfileImg},{ headers: user.headers });
+    if (response.status === 200) {
+      setReload(!reload);
+     
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+// const profileImageStyle = {
+//   width: "200px",
+//   height: "200px",
+//   borderRadius: "50%",
+// };
+
+// const inputStyle = {
+//   backgroundColor: "white",
+//   border: "1px solid #ccc",
+//   padding: "5px",
+//   marginBottom: "10px",
+//   marginLeft: "10px",
+// };
+
+// const editButtonStyle = {
+//   position: "absolute",
+//   bottom: "0",
+//   right: "0",
+//   backgroundColor: "transparent",
+//   border: "none",
+//   cursor: "pointer",
+// };
+
+// const editIconStyle = {
+//   width: "30px",
+//   height: "30px",
+// };
 
   return (
-    <div className="mx-auto">
-      <div className="flex bg-salmon justify-center max-sm:">
-        <img
-          className="flex relative top-[70px] w-[200px]  h-[200px] rounded-full sm:hidden"
-          src={user.profilePicture || defaultPicture}
-          alt="Profile"
-          // style={profileImageStyle}
-        />
-      </div>
-      {/* <div className="flex  bg-salmon  p-4 rounded-t-lg "> */}
-      <div className="flex bg-salmon items-end">
-        <div className=" w-1/2">
-          <img
-            className=" relative top-[70px] left-1/4 w-[200px]  h-[200px] rounded-full max-sm:hidden"
-            src={user.profilePicture || defaultPicture}
-            alt="Profile"
-            // style={profileImageStyle}
-          />
-        </div>
-        <div className=" w-1/2 text-white font-bold text-2xl max-sm:hidden">
-          <h1>Welcome, {user.userName}</h1>
-        </div>
-      </div>
+    <div className="md:w-1/2 mx-auto">
+        <div className="bg-salmon p-4  rounded-t-lg ">
+          <div className="flex justify-between items-center">
+      
+      
+              <div className="relative">
+              <img src={user.profilePicture} alt="Profile" className="w-48 h-48 rounded-full"/>
+             
+              {/* ส่วน modal update รูปภาพ  */}
+              <button className="absolute bottom-0 right-0 bg-transparent border-none cursor-pointer" onClick={()=>document.getElementById('my_modal_1').showModal()}>
+              <img src={editIcon} className="w-10 h-10" alt="editIcon" /></button>
+                  <dialog id="my_modal_1" className="modal">
+                  <div className="modal-box">
 
-      {/* </div> */}
+                  <h2 className="text-salmon font-bold text-2xl mb-5">Add New Profile Picture</h2>
+                  <img src={ProfileImg || ProfileURL} alt="venom-PROFILE" className="rounded-lg w-full h-auto" />
+                  <div className="field-value-pair mt-5">
+                  <labels>Enter image URL : </labels> 
+                  <input type="text" name="profilePicture" placeholder="image URL" value={ProfileImg} onChange={(ev) => setProfileImg(ev.target.value)}  className="bg-white border border-gray-300 p-5 mb-10 ml-10" />
+                    
+                  </div>
+                   
+                  <div className="modal-action">
+                  <button onClick={submitIMG} className="btn hover:btn-success" disabled={!ProfileImg}>
+                        Update
+                  </button>
+                  <form method="dialog">
+                   {/* if there is a button in form, it will close the modal */}
+                   <button className="btn btn-circle hover:btn-error">
+                          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                          </svg>
+                    </button>
+                  </form>
+                  </div>
+                  </div>
+                  </dialog>
+              </div>
+        
 
+        
+             <div className=" w-1/2 text-white font-bold text-2xl max-sm:hidden">
+               <h1>Welcome, {user.userName}</h1>
+              </div>
+           </div>
+       </div>
+
+      {/* ส่วนด้านล่าง */}
       <div className="bg-white p-4 rounded-b-lg pt-20 md:flex md:flex-row">
         <div className="md:flex-1">
           <h2 className="text-salmon font-bold text-2xl mb-5 text-center md:text-left max-sm:hidden">PERSONAL INFO</h2>
