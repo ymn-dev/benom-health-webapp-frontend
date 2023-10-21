775cdbb725222815feb1d839b84450026a19a033
sukandasaeieo <sukandasaeieo@gmail.com>
2023-10-21 19:28:24 +0700

    restyling landing page

--- src/Component/Home/Card.jsx
+++ src/Component/Home/Card.jsx
@@ -5,18 +5,18 @@ const Card = ({ Name, Image, Text }) => {
   const link = "/" + Name.split(" ").join("-").toLowerCase();
   return (
     
-    <div className="text-black  overflow-hidden mx-4  ">
-      <div className="bg-salmon p-4">
-        <h3 className="text-2xl font-family: 'Nunito' font-bold">{Name}</h3>
+    <div className="text-white overflow-hidden mx-4 hover:scale-105 shadow-md shadow-gray-950 duration-150 rounded-lg ">
+      <div className="bg-dark-sea p-4">
+        <h3 className="text-3xl font-family: 'Nunito' font-bold">{Name}</h3>
       </div>
-      <div>
-        <img src={Image} alt={Name} className="w-full" />
+      <div className="w-full h-2/4 overflow-hidden">
+        <img src={Image} alt={Name} className="w-full h-full object-cover" />
       </div>
-      <div className="bg-lightsalmon  p-4 pb-16">
-        <p className="text-black font-family: 'Nunito' mt-2 mb-8">{Text}</p>
-        <div className="card-actions justify-end">
-       <Link to={link} className=" text-blue-800 mt-2 font-bold just">
-          Learn more
+      <div className=" bg-slate-200 p-4 pb-20 ">
+        <p className="text-slate-700 font-family: 'Nunito' mt-2 mb-8">{Text}</p>
+        <div className="card-actions justify-center">
+       <Link to={link} className=" text-slate-700 mt-2 font-bold justify-center btn btn-wide rounded-full bg-slate-200 border-gray-400 hover:bg-sea-blue duration-500 hover:border-gray-400 hover:scale-105">
+         {'Learn more'}
         </Link>
     </div>
       </div>
--- src/Component/Home/CardContainer.jsx
+++ src/Component/Home/CardContainer.jsx
@@ -1,47 +1,47 @@
 import React from "react";
 import Card from "./Card";
-import yogaImgPath from "../../assets/yoga.png";
+import yogaImgPath from "../../assets/yoga02.jpg";
 import cyclingImgPath from "../../assets/cycling.png";
 import runningImgPath from "../../assets/running.png";
 import calisthenicsImgPath from "../../assets/calisthenics.png";
-import swimmingImgPath from "../../assets/swimming.png";
+import swimmingImgPath from "../../assets/swim02.jpg";
 import walkingImgPath from "../../assets/walking.png";
 
 const CardContainer = () => {
   return (
-    <div className="text-center py-8">
-      <h2 className="text-6xl text-white font-bold mb-5">Our Courses</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+    <div className="text-center py-5 bg-dark-blue">
+      <h2 className="text-5xl text-white font-bold mb-8 ">Our Courses</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
 
         <Card
           Name="Yoga"
           Image={yogaImgPath}
-          Text="Train your body, mind and spirit in a studio designed to suit everyone, with 3 different classes, utilizing ancient sciences and knowledge applied to the present"
+          Text="Train your body, mind and spirit in a studio designed to suit everyone, with 6 different classes, utilizing ancient sciences and knowledge applied to the present"
         />
         <Card
           Name="Cycling"
           Image={cyclingImgPath}
-          Text="Train your body, mind and spirit in a studio designed to suit everyone, with 3 different classes, utilizing ancient sciences and knowledge applied to the present"
+          Text="Train your body, mind and spirit in a studio designed to suit everyone, with 6 different classes, utilizing ancient sciences and knowledge applied to the present"
         />
         <Card
           Name="Running"
           Image={runningImgPath}
-          Text="Train your body, mind and spirit in a studio designed to suit everyone, with 3 different classes, utilizing ancient sciences and knowledge applied to the present"
+          Text="Train your body, mind and spirit in a studio designed to suit everyone, with 6 different classes, utilizing ancient sciences and knowledge applied to the present"
         />
         <Card
           Name="Calisthenics"
           Image={calisthenicsImgPath}
-          Text="Train your body, mind and spirit in a studio designed to suit everyone, with 3 different classes, utilizing ancient sciences and knowledge applied to the present"
+          Text="Train your body, mind and spirit in a studio designed to suit everyone, with 6 different classes, utilizing ancient sciences and knowledge applied to the present"
         />
         <Card
           Name="Swimming"
           Image={swimmingImgPath}
-          Text="Train your body, mind and spirit in a studio designed to suit everyone, with 3 different classes, utilizing ancient sciences and knowledge applied to the present"
+          Text="Train your body, mind and spirit in a studio designed to suit everyone, with 6 different classes, utilizing ancient sciences and knowledge applied to the present"
         />
         <Card
           Name="Walking"
           Image={walkingImgPath}
-          Text="Train your body, mind and spirit in a studio designed to suit everyone, with 3 different classes, utilizing ancient sciences and knowledge applied to the present"
+          Text="Train your body, mind and spirit in a studio designed to suit everyone, with 6 different classes, utilizing ancient sciences and knowledge applied to the present"
         />
       </div>
     </div>
