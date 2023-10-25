55be7d6f65c3e4653f2046152d65973d5acfcc04
fiadaze <fiakantapon@gmail.com>
2023-10-25 16:33:47 +0700

    fix card responsive for background

--- src/Component/Home/Card.jsx
+++ src/Component/Home/Card.jsx
@@ -5,22 +5,30 @@ const Card = ({ Name, Image, Text }) => {
   const link = "/" + Name.split(" ").join("-").toLowerCase();
   return (
     
-    <div className="text-white overflow-hidden mx-4 hover:scale-105 shadow-md shadow-gray-950 duration-150 rounded-lg ">
-      <div className="bg-dark-sea p-4">
-        <h3 className="text-3xl font-family: 'Nunito' font-bold">{Name}</h3>
-      </div>
-      <div className="w-full h-2/4 overflow-hidden">
-        <img src={Image} alt={Name} className="w-full h-full object-cover" />
-      </div>
-      <div className=" bg-slate-200 p-4 pb-20 ">
-        <p className="text-slate-700 font-family: 'Nunito' mt-2 mb-8">{Text}</p>
-        <div className="card-actions justify-center">
-       <Link to={link} className=" text-slate-700 mt-2 font-bold justify-center btn btn-wide rounded-full bg-slate-200 border-gray-400 hover:bg-sea-blue duration-500 hover:border-gray-400 hover:scale-105">
-         {'Learn more'}
-        </Link>
+    <div className="zoom-on-hover overflow-hidden mx-auto shadow-md shadow-gray-950 duration-150 rounded-lg">
+  <div className="bg-dark-sea p-4">
+    <h3 className="text-3xl font-family: 'Nunito' font-bold">{Name}</h3>
+  </div>
+  <div className="w-full overflow-hidden">
+    <img src={Image} alt={Name} className="w-full h-full object-cover" />
+  </div>
+  <div className="bg-slate-200 h-full  flex-col justify-between space-y-2 pb-5">
+    <div className=" h-fit">
+      <p className="text-slate-700  m-4 font-family: 'Nunito' ">{Text}</p>
     </div>
-      </div>
+    <div className="card-actions justify-center  ">
+      <Link
+        to={link}
+        className="  text-slate-700 mt-2 font-bold btn btn-wide rounded-full bg-slate-200 border-gray-400 hover:bg-sea-blue duration-500 hover:border-gray-400 hover:scale-105"
+        
+      >
+        {'Learn more'}
+      </Link>
     </div>
+  </div>
+  
+</div>
+
     
     // <div className="bg-lightsalmon rounded overflow-hidden shadow-lg">
     //   <img className="w-full" src={Image} alt={Name}/>
